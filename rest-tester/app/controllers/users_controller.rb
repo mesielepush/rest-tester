101c20c5674a903b9d22d3f565b44a623cd51890
mesielepush <48403308+mesielepush@users.noreply.github.com>
2019-11-22 09:58:33 -0600

    rest requestor

--- rest-tester/app/controllers/users_controller.rb
+++ rest-tester/app/controllers/users_controller.rb
@@ -1,8 +1,21 @@
 class UsersController < ApplicationController
-    def  index
-        @name =  "I am the Index action"
+    def index
+        @name = "I am the Index action!"
     end
+
+    def new
+        @name = "I am the new action!"
+    end
+
+    def edit
+        @name = "I am the edit action!"
+    end
+
+    def show
+        @name = "I am the show action!"
+    end
+
     def create
     end
-    
+
 end
--- rest-tester/app/script/rest_requestor.rb
+++ rest-tester/app/script/rest_requestor.rb
@@ -1,5 +1,19 @@
 require 'rest-client'
-  url = "http://localhost:3000"
 
-  puts RestClient.get(url)
-  RestClient.post(url,"")
+puts 'Insert the Url index'
+url = gets.chomp
+puts RestClient.get(url)
+RestClient.post(url,"")
+
+
+puts 'Insert the Url new'
+url = gets.chomp
+puts RestClient.get(url)
+
+puts 'Insert the Url edit'
+url = gets.chomp
+puts RestClient.get(url)
+
+puts 'Insert the Url show'
+url = gets.chomp
+puts RestClient.get(url)
